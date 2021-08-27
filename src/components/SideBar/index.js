af881a9466706d4361ebe075d4b115b90ffe6b6c
jhhg04 <jhhg04@gmail.com>
2021-08-27 07:29:30 -0500

    first commit

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,13 @@
+import { Icon, CloseIcon, SidebarContainer } from "./SidebarElements";
+
+const SideBar = () => {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+    </SidebarContainer>
+  );
+};
+
+export default SideBar;
